79ad0555dedfcb7f6641ccb881538adebe4f43c0
keemaan <keemaan@naver.com>
2021-09-03 11:49:57 +0900

    SimpleDateFormat의 parse 기능 활용 -> String으로 저장된 생년월일을 Calendar에 저장

--- PizzaOrderApp_20210825-master-revise-version-20210826-12-47/PizzaOrderApp_20210825-master-revise-version-20210826-12-47/app/src/main/java/com/neppplus/pizzaorderapp_20210825/datas/StoreData.kt
+++
@@ -1,8 +0,0 @@
-package com.neppplus.pizzaorderapp_20210825.datas
-
-import java.io.Serializable
-
-class StoreData(
-    val name : String,
-    val phoneNum : String,
-    val logoURL: String) : Serializable {}
--- PizzaOrderApp_20210825-master-revise-version-20210826-12-47/PizzaOrderApp_20210825-master-revise-version-20210826-12-47/app/src/main/java/com/neppplus/pizzaorderapp_20210825/datas/StoreData_chicken.kt
+++
@@ -1,8 +0,0 @@
-package com.neppplus.pizzaorderapp_20210825.datas
-
-import java.io.Serializable
-
-class StoreData_chicken(
-    val name : String,
-    val phoneNum : String,
-    val logoURL: String) : Serializable {}
